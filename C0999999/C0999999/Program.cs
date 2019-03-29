522a638d318e26ec983936f2bb90bcfb95473bb7
Peter Sigurdson <Peter.Sigurdson@Tor.Loc>
2019-03-29 18:40:41 -0400

    first

---
+++ C0999999/C0999999/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0999999
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		HelloWorld hw=new HelloWorld();
+		hw.Hello();
+        }
+    }
+    class HelloWorld
+    {
+        public void Hello()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
